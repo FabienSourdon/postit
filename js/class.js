165677a8afff6be9b2db69245bbaf7f343f63945
Fabien Sourdon <fabien.sourdon@gmail.com>
2020-06-17 08:32:29 +0200

    Modif16062020

--- js/class.js
+++ js/class.js
@@ -21,101 +21,172 @@ class postit{
         this.Contenu = contentPost;
     }
 
+    // Fonction de création de postit
     createPostit(){
-    
+        
         let divContainer = document.getElementById('contPost');
-        let containPost = document.getElementById('postit'+this.Ident);
+        let containPost = document.getElementById('postit'+parseInt(this.Ident));
     
         if(containPost === null){
             
             containPost = document.createElement('div');
-            containPost.id = 'postit'+this.Ident;
+            containPost.id = 'postit'+parseInt(this.Ident++);
         }
     
         containPost.style.position = 'absolute';
         containPost.style.zIndex = this.PosZ;
-        containPost.style.top = this.PosX;
-        containPost.style.left = this.PosY;
-        containPost.style.height = this.Long;
-        containPost.style.width = this.Larg;
+        containPost.style.top = this.PosX+"px";
+        containPost.style.left = this.PosY+"px";
+        containPost.style.height = this.Long+"px";
+        containPost.style.width = this.Larg+"px";
         containPost.style.backgroundColor = this.CouleurFond;
         containPost.style.color = this.CouleurTexte;
         containPost.style.boxShadow = "5px 5px 35px #000000";
         containPost.style.display = "flex";
-        containPost.style.flexFlow = "column-reverse wrap"
+        containPost.style.flexFlow = "column-reverse wrap";
         containPost.style.alignItems = "center";
         containPost.style.justifyContent = "space-between";
         
         let getP = document.getElementById('pId'+this.Ident);
         let contButon = document.createElement('div');
 
+
         if(getP == null){
+
+            //Creation du texte
             let pPostit = document.createElement('p');
-            pPostit.id = "pId"+this.Ident;
+            pPostit.id = "pId"+this.Ident++;
             pPostit.style.paddingTop = "45%";
             pPostit.innerHTML = this.Contenu;
+            pPostit.contentEditable = true;
             
-
+            //Creation du bouton move
             let imgMove = document.createElement('img');
             imgMove.src = "./img/move.svg";
-            imgMove.style.height = "15px";
-            imgMove.style.width = "15px";
+            imgMove.style.height = "30px";
+            imgMove.style.width = "30px";
 
             let btnMove = document.createElement('button');
-            btnMove.style.height = "15px";
-            btnMove.style.width = "15px";
+            btnMove.id = "btnMove"+parseInt(this.Ident++);
+            btnMove.style.height = "30px";
+            btnMove.style.width = "30px";
             btnMove.style.border = "none";
             btnMove.style.backgroundColor = this.CouleurFond;
             btnMove.style.marginLeft = "5px";
+            
+                //Ajout de listener sur le bouton move
+                var mousePosition;
+                var offset = [this.PosX,this.PosY];
+                var isDown = false;
 
+                btnMove.addEventListener('dragstart', function(e){
+                    isDown = true;
+                    offset = [
+                        containPost.offsetLeft - e.clientX,
+                        containPost.offsetTop - e.clientY
+                    ];
+                });
+
+                btnMove.addEventListener('dragend', function() {
+                    isDown = false;
+                    containPost.posX = containPost.style.left ;
+                    containPost.posY = containPost.style.top ;
+                });
+
+                btnMove.addEventListener('drag', function(event) {
+                    event.preventDefault();
+                    if (isDown) {
+                        mousePosition = {
+            
+                        x : event.clientX,
+                        y : event.clientY
+            
+                        };
+                    containPost.style.left = (mousePosition.x + offset[0]) + 'px';
+                    containPost.style.top  = (mousePosition.y + offset[1]) + 'px';
+                }
+                });
+
+            //Creation du bouton delete
             let imgDel = document.createElement('img');
             imgDel.src = "./img/delete.svg";
-            imgDel.style.height = "15px";
-            imgDel.style.width = "15px";
+            imgDel.style.height = "30px";
+            imgDel.style.width = "30px";
 
             let btnDel = document.createElement('button');
-            btnDel.style.height = "15px";
-            btnDel.style.width = "15px";
+            btnDel.style.height = "30px";
+            btnDel.style.width = "30px";
             btnDel.style.border = "none";
             btnDel.style.backgroundColor = this.CouleurFond;
             btnDel.style.marginLeft = "5px";
 
+            //Creation du bouton resize
             let imgResize = document.createElement('img');
             imgResize.src = "./img/maximize-2.svg";
-            imgResize.style.height = "15px";
-            imgResize.style.width = "15px";
+            imgResize.style.height = "30px";
+            imgResize.style.width = "30px";
 
             let btnResize = document.createElement('button');
-            btnResize.style.height = "15px";
-            btnResize.style.width = "15px";
+            btnResize.id = "btnResize"+this.Ident;
+            btnResize.style.height = "30px";
+            btnResize.style.width = "30px";
             btnResize.style.border = "none";
             btnResize.style.backgroundColor = this.CouleurFond;
             btnResize.style.marginLeft = "5px";
+            btnResize.addEventListener('dragstart', initDrag, false);
 
+                //creation Resize
+                var startX, startY, startWidth, startHeight;
+
+                function initDrag(e){
+                    startX = e.clientX;
+                    startY = e.clientY;
+                    startWidth = parseInt(containPost.style.width);
+                    startHeight = parseInt(containPost.style.height);
+                    btnResize.addEventListener('drag', drag);
+                    btnResize.addEventListener('dragend', stopDrag);
+                    console.log(parseInt(containPost.style.height),parseInt(containPost.style.width));
+                }
+
+                function drag(e){
+                    containPost.style.width = startWidth + (e.clientX - startX) + 'px';
+                    containPost.style.height = startHeight + (e.clientY - startY) + 'px';
+                    console.log(containPost.style.height);
+                }
+
+                function stopDrag(){
+                    btnResize.removeEventListener('drag', drag);
+                    btnResize.removeEventListener('dragend', stopDrag);
+                    console.log(parseInt(containPost.style.height),parseInt(containPost.style.width));
+                }
+                
+            //Creation du bouton edit
             let imgEdit = document.createElement('img');
             imgEdit.src = "./img/edit.svg";
-            imgEdit.style.height = "15px";
-            imgEdit.style.width = "15px";
+            imgEdit.style.height = "30px";
+            imgEdit.style.width = "30px";
 
             let btnEdit = document.createElement('button');
-            btnEdit.style.height = "15px";
-            btnEdit.style.width = "15px";
+            btnEdit.style.height = "30px";
+            btnEdit.style.width = "30px";
             btnEdit.style.border = "none";
             btnEdit.style.backgroundColor = this.CouleurFond;
             btnEdit.style.marginLeft = "5px";
 
+            //Creation du bouton axeZ
             let imgZidx = document.createElement('img');
             imgZidx.src = "./img/layers.svg";
-            imgZidx.style.height = "15px";
-            imgZidx.style.width = "15px";
+            imgZidx.style.height = "30px";
+            imgZidx.style.width = "30px";
 
             let btnZidx = document.createElement('button');
-            btnZidx.style.height = "15px";
-            btnZidx.style.width = "15px";
+            btnZidx.style.height = "30px";
+            btnZidx.style.width = "30px";
             btnZidx.style.border = "none";
             btnZidx.style.backgroundColor = this.CouleurFond;
             btnZidx.style.marginLeft = "5px";
 
+            //Ajout des éléments créés au DOM
             btnMove.appendChild(imgMove);
             btnDel.appendChild(imgDel);
             btnResize.appendChild(imgResize);
@@ -126,8 +197,8 @@ class postit{
             contButon.appendChild(btnResize);
             contButon.appendChild(btnEdit);
             contButon.appendChild(btnZidx);
-            contButon.appendChild(pPostit);
             containPost.appendChild(contButon);
+            containPost.appendChild(pPostit);
         }
         else{
             getP.innerHTML = this.Contenu;
@@ -135,28 +206,6 @@ class postit{
         }
     
         divContainer.appendChild(containPost);
-    }
-
-    movePostit(newposX,newposY){
-        this.PosX = newposX+"px";
-        this.PosY = newposY+"px";
-    }
-
-    resizePostit(newLong, newLarg){
-        this.Long = newLong;
-        this.Larg = newLarg;
-    }
-
-    editPostit(newEntry){
-        this.Contenu = newEntry;
-    }
-
-    deleteContent(){
-        this.Contenu = "";
-    }
-
-    changePostitZ(newz){
-        this.PosZ = newz;
     }
 }
 
--- js/postit.js
+++ js/postit.js
@@ -1,18 +1,12 @@
-let postit1 = new postit("0","0","0","10","300px","300px","#04B45F","#000000","Important : Révolutionner Javascript")
-let postit2 = new postit("1","0","0","10","300px","300px","#cf1818","#FFFFFF","Mettre un terme a PHP")
-let postit3 = new postit("2","0","0","10","300px","300px","#354cb4","#FFFFFF","Coder au bord de la piscine")
+let postit1 = new postit("0","6","390","10","300","300","#04B45F","#000000","Important : Révolutionner Javascript")
+let postit2 = new postit("1","6","790","10","300","300","#cf1818","#FFFFFF","Mettre un terme a PHP")
+let postit3 = new postit("2","6","1190","10","300","300","#354cb4","#FFFFFF","Coder au bord de la piscine")
 
+let createVert = document.getElementById('postitVert');
+createVert.addEventListener('click', function(){postit1.createPostit()});
 
-postit1.createPostit();
-postit2.createPostit();
-postit3.createPostit();
-//postit1.resizePostit("500px", "500px");
-postit1.movePostit("450", "710");
-postit2.movePostit("250", "410");
-postit3.movePostit("550", "1110");
-//postit1.deleteContent()
-//postit1.editPostit("blablabla")
-//postit1.changePostitZ("8")
-postit1.createPostit();
-postit2.createPostit();
-postit3.createPostit();
+let createRouge = document.getElementById('postitRouge');
+createRouge.addEventListener('click', function(){postit2.createPostit()});
+
+let createBleu = document.getElementById('postitBleu');
+createBleu.addEventListener('click', function(){postit3.createPostit()});
